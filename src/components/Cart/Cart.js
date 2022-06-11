100d51731b0698ee639ba864176c3b87547a22f0
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-11 18:10:17 +0530

    checkout form added and cart modal updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Modal from '../UI/Model';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext);
+    const [isCheckout, setIsCheckout] = useState(false)
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
@@ -33,6 +35,16 @@ const Cart = (props) => {
             ))}
         </ul>
     );
+    const orderHandler = () => {
+        setIsCheckout(true)
+    }
+
+    const modalActions = <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+            Close
+        </button>
+        {hasItems && <button onClick={orderHandler} className={classes.button}>Order</button>}
+    </div>
 
     return (
         <Modal onClose={props.onClose}>
@@ -41,12 +53,9 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>
-                    Close
-                </button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {!isCheckout && modalActions}
+
         </Modal>
     );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-import { Spinner } from "react-bootstrap"
+import { Spinner, Container } from "react-bootstrap"
 
 
 
@@ -10,15 +10,21 @@ function AvailableMeals() {
 
     const [meals, setMeals] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState();
+
 
     useEffect(() => {
         setIsLoading(true)
         const fetchMeals = async () => {
             const response = await fetch('https://food-app-react-180ca-default-rtdb.firebaseio.com/meals.json')
+
+            if (!response.ok) {
+                throw new Error("something went wrong!")
+            }
+
             const responseData = await response.json();
 
             const loadedData = []
-
             for (const key in responseData) {
                 loadedData.push({
                     id: key,
@@ -29,22 +35,36 @@ function AvailableMeals() {
             }
             setMeals(loadedData)
             setIsLoading(false)
-
         }
 
-        fetchMeals();
+        fetchMeals().catch((error) => {
+            setIsLoading(false)
+            setError(error.message)
+        })
+
     }, [])
 
+    if (isLoading) {
+        return <div className={classes.mealsLoading}>
+            <Card >
+                <Spinner animation='border' />
+            </Card>
+        </div>
+    }
+
+    if (error) {
+        return <div className={classes.error}>
+            <Card>
+                <h1>{error}</h1>
+            </Card>
+        </div>
+    }
 
 
     const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
 
     return <section className={classes.meals}>
         <Card>
-            <div className={classes.mealsLoading}>
-                {isLoading && <Spinner animation='border' />}
-            </div>
-
             <ul>
                 {mealsList}
             </ul>
